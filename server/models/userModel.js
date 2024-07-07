df4d6721b3d9ead40562bca451b1e24b7197f83c
ahme1kash <kashifrichy@gmail.com>
2024-07-07 23:44:44 +0530

    Adds confirmPassword in user model.

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -21,7 +21,12 @@ const userSchema = new mongoose.Schema(
         password: {
             type: String,
             required: [true, "Password is Required"],
-        }
+        },
+        confirmPassword: {
+            type: String,
+            required: [true, "Password is Required"],
+        },
+
     },
     { timestamps: true }
 );
